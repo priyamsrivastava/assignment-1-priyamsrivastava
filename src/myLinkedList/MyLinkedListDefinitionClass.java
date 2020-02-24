d32ba3e51f8658a5b751ed0e65c097b32862acfe
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-02-24 21:41:18 +0530

    created Node class

--- src/myLinkedList/MyLinkedListDefinitionClass.java
+++ src/myLinkedList/MyLinkedListDefinitionClass.java
@@ -1,4 +1,34 @@
 package myLinkedList;
 
-public class MyLinkedListDefinitionClass<E> {
+public class MyLinkedListDefinitionClass<E> implements MyLinkedListADT {
+
+    @Override
+    public void add(java.lang.Object item) {
+
+    }
+
+    @Override
+    public java.lang.Object remove() {
+        return null;
+    }
+
+    @Override
+    public int search(java.lang.Object item) {
+        return 0;
+    }
+
+    @Override
+    public MyLinkedListDefinitionClass sort(MyLinkedListDefinitionClass list) {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void swapAdjacentElements(Node firstNode, Node secondNode, int indexOfNodeBeforeFirstNode) {
+
+    }
 }
