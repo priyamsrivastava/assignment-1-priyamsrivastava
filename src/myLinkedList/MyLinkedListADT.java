9784f77df088cac87f67e7a2063ae2bb2e0c7fcf
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-02-24 21:36:16 +0530

    created methods in linked list ADT

--- src/myLinkedList/MyLinkedListADT.java
+++ src/myLinkedList/MyLinkedListADT.java
@@ -1,5 +1,17 @@
 package myLinkedList;
 
-public interface MyLinkedListADT {
+public interface MyLinkedListADT<E> {
+    void add(E item);
 
+    E remove();
+
+    int search(E item);
+
+
+    MyLinkedListDefinitionClass<E> sort(MyLinkedListDefinitionClass<E> list);
+
+
+    void swapAdjacentElements(Node<E> firstNode, Node<E> secondNode, int indexOfNodeBeforeFirstNode);
+
+    void print();
 }
