be2b11e5dab0fd891c391ed8ea4d3a58c31e4a68
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-02-24 21:42:45 +0530

    created constructor

--- src/myLinkedList/Node.java
+++ src/myLinkedList/Node.java
@@ -3,4 +3,9 @@ package myLinkedList;
 public class Node<E> {
     private E data;
     private Node<E> next;
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
