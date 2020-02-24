26f321ff333b08ea8b9fa7c146460ec40c2d1268
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-02-24 21:46:57 +0530

    created getNode method. It returns the next reference of the node of the given index.

--- src/myLinkedList/MyLinkedListDefinitionClass.java
+++ src/myLinkedList/MyLinkedListDefinitionClass.java
@@ -4,6 +4,15 @@ public class MyLinkedListDefinitionClass<E> implements MyLinkedListADT {
     private Node<E> head = null;
     private int size;
 
+    public Node<E> getNode(int index) {
+        Node<E> response = head;
+
+        for (int i = 0; i < index; i++) {
+            response = response.getNext();
+        }
+        return response;
+    }
+
     @Override
     public void add(java.lang.Object item) {
 
